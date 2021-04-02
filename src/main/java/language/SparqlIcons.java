ed7bc95fcc37ccbe09d0c94f6a74404d7484f7d7
marcelhillesheim <marcelhillesheim3@web.de>
2021-04-02 14:26:25 +0200

    added file icon

--- src/main/java/language/SparqlIcons.java
+++ src/main/java/language/SparqlIcons.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 
 public class SparqlIcons {
 
-    public static final Icon FILE = IconLoader.getIcon("/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/icons/sparql-file.svg", SparqlIcons.class);
 
 }
