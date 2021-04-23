ddef9f789c0ed643efecacd9ef84818d12607efe
marcelhillesheim <marcelhillesheim3@web.de>
2021-04-23 15:45:36 +0200

    added information text on how to use prefix settings

--- src/main/java/ui/SparqlPrefixSettingsDialog.java
+++ src/main/java/ui/SparqlPrefixSettingsDialog.java
@@ -2,6 +2,7 @@ package ui;
 
 import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.table.JBTable;
+import com.intellij.util.ui.FontInfo;
 import settings.SparqlAppSettingsManager;
 
 import javax.swing.*;
@@ -18,6 +19,20 @@ public class SparqlPrefixSettingsDialog {
         this.rootPanel = new JPanel();
         rootPanel.setLayout(new BorderLayout());
         rootPanel.add(generateTable(), BorderLayout.CENTER);
+
+        String informationText = "Here you can set Prefixes, which are being used for annotations and quickfixes. " +
+                "If a prefix is set to standard, you won't get a warning, if the prefix is used but not declared within the query. " +
+                "\n \n Disclaimer: For the query execution tool the standard prefixes are always added automatically. " +
+                "So if the library, you are using for query execution, requires all Prefixes to be defined within the query, " +
+                "the query might fail when running via your program. In this case you should set all prefixes to non standard. " +
+                "So you get warnings, if prefix declarations are missing.";
+        JTextArea informationTextArea = new JTextArea(informationText);
+        informationTextArea.setEditable(false);
+        informationTextArea.setLineWrap(true);
+        informationTextArea.setWrapStyleWord(true);
+        informationTextArea.setOpaque(false);
+        informationTextArea.setFont(FontInfo.get(Font.DIALOG).getFont());
+        rootPanel.add(informationTextArea, BorderLayout.NORTH);
     }
 
     private JComponent generateTable() {
