1892a7fcee6b2c2ec596bc27c28322ebbd72aa95
marcelhillesheim <marcelhillesheim3@web.de>
2021-08-10 00:29:17 +0200

    added assert and fixed problem with given sample query

--- src/test/java/language/SparqlSmartCompletionTest.java
+++ src/test/java/language/SparqlSmartCompletionTest.java
@@ -7,17 +7,19 @@ import settings.SparqlAppSettingsManager;
 import java.util.List;
 
 public class SparqlSmartCompletionTest extends LightJavaCodeInsightFixtureTestCase {
-    //TODO add asserts
+
     public void testCompletionTriple() {
 
-        String querySelect1 = "SELECT ?a {" +
-                "?a <caret>}";
-        myFixture.configureByText(SparqlFileType.INSTANCE, querySelect1);
+        String querySelect = "SELECT ?a {" +
+                "?a ?b <caret>}";
+        myFixture.configureByText(SparqlFileType.INSTANCE, querySelect);
         SparqlAppSettingsManager.getInstance().endpointSettingsForExecution.setUrl("http://dbpedia.org/sparql");
 
         myFixture.complete(CompletionType.SMART);
         List<String> lookupElementStrings = myFixture.getLookupElementStrings();
         System.out.println(lookupElementStrings);
+        assert lookupElementStrings != null;
+        assertFalse(lookupElementStrings.isEmpty());
 
     }
 }
